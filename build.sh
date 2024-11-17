c680439b4c461177797b479b1f43ad40abb80a6f
kyle <75395106+ehre9305@users.noreply.github.com>
2024-11-17 03:14:22 -0600

    runner

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+rm -rf build dist
 pyinstaller --onefile \
   --add-data "$VIRTUAL_ENV/lib/python3.9/site-packages/mediapipe/modules/pose_landmark/pose_landmark_full.tflite:mediapipe/modules/pose_landmark" \
   --add-data "$VIRTUAL_ENV/lib/python3.9/site-packages/mediapipe/modules/pose_detection/pose_detection.tflite:mediapipe/modules/pose_detection" \
   --add-data "$VIRTUAL_ENV/lib/python3.9/site-packages/mediapipe/modules/pose_landmark/pose_landmark_cpu.binarypb:mediapipe/modules/pose_landmark" \
+  --clean \
   --hidden-import="mediapipe.python.solutions.pose" main.py
