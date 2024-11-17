610f8ee27e9ef73e56e04d2a0a8b7a3055754f44
kyle <75395106+ehre9305@users.noreply.github.com>
2024-11-17 02:26:09 -0600

    generalize installer

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pyinstaller --onefile \
-  --add-data "./.venv/lib/python3.9/site-packages/mediapipe/modules/pose_landmark/pose_landmark_full.tflite:mediapipe/modules/pose_landmark" \
-  --add-data "./.venv/lib/python3.9/site-packages/mediapipe/modules/pose_detection/pose_detection.tflite:mediapipe/modules/pose_detection" \
-  --add-data "./.venv/lib/python3.9/site-packages/mediapipe/modules/pose_landmark/pose_landmark_cpu.binarypb:mediapipe/modules/pose_landmark" \
+  --add-data "$VIRTUAL_ENV/lib/python3.9/site-packages/mediapipe/modules/pose_landmark/pose_landmark_full.tflite:mediapipe/modules/pose_landmark" \
+  --add-data "$VIRTUAL_ENV/lib/python3.9/site-packages/mediapipe/modules/pose_detection/pose_detection.tflite:mediapipe/modules/pose_detection" \
+  --add-data "$VIRTUAL_ENV/lib/python3.9/site-packages/mediapipe/modules/pose_landmark/pose_landmark_cpu.binarypb:mediapipe/modules/pose_landmark" \
   --hidden-import="mediapipe.python.solutions.pose" main.py
